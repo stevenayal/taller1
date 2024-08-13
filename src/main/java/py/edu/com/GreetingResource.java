e60fac0489e1c63049a785e6559940d8b50a92dc
Steven Ayala <stevengilbertayala@gmail.com>
2024-08-12 20:58:47 -0400

    se agrega metodos de suma y resta

--- src/main/java/py/edu/com/GreetingResource.java
+++ src/main/java/py/edu/com/GreetingResource.java
@@ -3,14 +3,24 @@ package py.edu.com;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+
+@Path("/recursos-basicos")
 public class GreetingResource {
 
     @GET
+    @Path("/suma")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello from Quarkus REST";
+    public int sum(@QueryParam("a") int a, @QueryParam("b") int b) {
+        return a + b;
+    }
+
+    @GET
+    @Path("/resta")
+    @Produces(MediaType.TEXT_PLAIN)
+    public int subtract(@QueryParam("a") int a, @QueryParam("b") int b) {
+        return a - b;
     }
 }
